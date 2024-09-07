5f5adfd8f4638c114ccf64b0243be2449b23657a
Mathew Joseph t a <144910668+mathewjosephta@users.noreply.github.com>
2024-09-07 20:28:37 +0530

    Create PalindromeStringCheck6.java

---
+++ PalindromeStringCheck6.java
@@ -0,0 +1,42 @@
+//EXPERIMENT 6
+
+/* write a java program to check whether a STRING is palindrome or not */
+
+import java.util.Scanner;
+
+public class PalindromeStringCheck6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from the user
+        System.out.print("Enter a string: ");
+        String originalString = sc.nextLine();
+
+        // Convert the string to lowercase to ignore case sensitivity
+        String str = originalString.toLowerCase();
+
+        // Reversing the string
+        String reverseString = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverseString += str.charAt(i);
+        }
+
+        // Checking if the original string and reversed string are the same
+        if (str.equals(reverseString)) {
+            System.out.println(originalString + " is a palindrome.");
+        } else {
+            System.out.println(originalString + " is not a palindrome.");
+        }
+
+        // Closing the scanner
+        sc.close();
+    }
+}
+
+//SAMPLE OUTPUT
+/*
+Enter a string: Madam
+
+Madam is a palindrome.
+
+*/
