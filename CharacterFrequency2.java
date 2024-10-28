2510a334b5f8dbc413f611339a866fc2137d4dc2
Mathew Joseph T A <144910668+mathewjosephta@users.noreply.github.com>
2024-10-28 18:17:38 +0530

    Update and rename CharacterFrequency8.java to CharacterFrequency2.java

--- CharacterFrequency2.java
+++ CharacterFrequency2.java
@@ -1,4 +1,4 @@
-// EXPERIMENT 8
+// EXPERIMENT 2
 
 /* Write a Java Program to find the frequency of a given character in a string. */
 
@@ -10,23 +10,23 @@ public class CharacterFrequency {
 
         // Taking input from the user
         System.out.print("Enter a string: ");
-        String inputString = sc.nextLine();
+        String str = sc.nextLine();
 
         System.out.print("Enter the character to find its frequency: ");
-        char characterToFind = sc.next().charAt(0);
+        char ch = sc.next().charAt(0); // Reads a single character input from the user
 
         // Initialize the frequency counter
         int frequency = 0;
 
         // Loop through the string to count occurrences of the given character
-        for (int i = 0; i < inputString.length(); i++) {
-            if (inputString.charAt(i) == characterToFind) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
                 frequency++;
             }
         }
 
         // Print the result
-        System.out.println("Frequency of '" + characterToFind + "' in the string is: " + frequency);
+        System.out.println("Frequency of '" + ch + "' in the string is: " + frequency);
 
         // Closing the scanner
         sc.close();
