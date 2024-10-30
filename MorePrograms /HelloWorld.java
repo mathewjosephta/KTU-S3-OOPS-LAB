47552814ea0b2fab2e7c0134e2a0c850625f034b
Mathew Joseph T A <144910668+mathewjosephta@users.noreply.github.com>
2024-10-30 20:37:29 +0530

    Create HelloWorld.java

---
+++ MorePrograms /HelloWorld.java
@@ -0,0 +1,10 @@
+/* Write a java program to print "Hello World" */
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+//SAMPLE OUTPUT
+/* Hello, World! */
