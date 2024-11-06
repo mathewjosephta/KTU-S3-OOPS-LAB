86cbcf8f16a838ed4a63651a54dd8228e3e7bb4a
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2024-11-06 10:01:56 +0530

    Update FileHandling7.java

--- FileHandling7.java
+++ FileHandling7.java
@@ -5,7 +5,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class FileHandlingMenu{
+public class FileHandling7{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
